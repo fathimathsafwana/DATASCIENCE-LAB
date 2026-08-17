bc2f543ed00b511a4eb90f1226f4b55356c8d83b
fathimathsafwana <fathimathsafwanatayattuchira@gmail.com>
2026-08-17 15:07:57 +0530

    Update p11.py

--- p11.py
+++ p11.py
@@ -1,24 +1,13 @@
 import pandas as pd
-
-data = {
-    'Name': ['Asha', 'Ravi', 'Anu', 'Rahul'],
-    'Age': [20, 22, 19, 21],
-    'Marks': [80, 90, 75, 85]
+data={
+    'name':['ali','sara','anu','ramu'],
+    'age':[21,15,24,27],
+    'marks':[85,90,78,88]
 }
-
-df = pd.DataFrame(data)
-
-print("Original DataFrame:")
+df=pd.DataFrame(data)
+print("original DataFrame")
 print(df)
-
-# Filter rows where Marks > 80
-print("\nMarks greater than 80:")
-print(df.loc[df['Marks'] > 80])
-
-# Select Name and Marks columns
-print("\nName and Marks:")
-print(df.loc[:, ['Name', 'Marks']])
-
-# Filter rows and select specific columns
-print("\nStudents with Marks > 80:")
-print(df.loc[df['Marks'] > 80, ['Name', 'Marks']])
+print("\nStudents with marks>80:")
+print(df[df['marks']>80])
+print("\nSelected data using .loc:")
+print(df.loc[0:2,['name','marks']])
